cdb5633d7136b02f4194a5f926374f70aa71b7e1
Tristan Beuve <Tristan.beuve@gmail.com>
2024-04-03 09:58:40 +0200

    fix .env

--- src/Controller/BaieController.php
+++ src/Controller/BaieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ReservationRepository;
 use App\Repository\UserRepository;
+use ContainerXnxxe4H\getBaieControllerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
