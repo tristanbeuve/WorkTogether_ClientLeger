9484a66a6fae888b390fa55934da6ee866500e93
Tristan Beuve <Tristan.beuve@gmail.com>
2024-04-08 16:28:54 +0200

    feat: test sentry

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,10 +12,25 @@ use App\Service\Redis;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     #[Route('/home', name: 'app_home')]
     public function index(UserRepository $ur, ReservationRepository $rr): Response
     {
-        return $this->render('home/index.html.twig', [
-        ]);
+        // the following code will test if monolog integration logs to sentry
+        $this->logger->error('My custom logged error.');
+
+        // the following code will test if an uncaught exception logs to sentry
+        throw new \RuntimeException('Example exception.');
+
+//        return $this->render('home/index.html.twig', [
+//        ]);
     }
 }
