f71bc94b92e910aeeb034a2657bd77506fe01c35
Tristan Beuve <Tristan.beuve@gmail.com>
2024-04-08 16:34:17 +0200

    fix: test sentry

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Repository\ReservationRepository;
 use App\Repository\UserRepository;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,25 +11,10 @@ use App\Service\Redis;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
     #[Route('/home', name: 'app_home')]
     public function index(UserRepository $ur, ReservationRepository $rr): Response
     {
-        // the following code will test if monolog integration logs to sentry
-        $this->logger->error('My custom logged error.');
-
-        // the following code will test if an uncaught exception logs to sentry
-        throw new \RuntimeException('Example exception.');
-
-//        return $this->render('home/index.html.twig', [
-//        ]);
+        return $this->render('home/index.html.twig', [
+        ]);
     }
 }
